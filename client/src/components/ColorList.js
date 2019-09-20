a095db8cd8879bdf037f8a6bbc7f7905ec43db91
Chris Bonifacio <christopher.bonifacio@gmail.com>
2019-09-20 12:03:08 -0400

    build out saveEdit function

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,31 +1,39 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React, { useState } from "react"
+import { axiosWithAuth } from "../utils/axiosWithAuth"
 
 const initialColor = {
   color: "",
   code: { hex: "" }
-};
+}
 
 const ColorList = ({ colors, updateColors }) => {
-  console.log(colors);
-  const [editing, setEditing] = useState(false);
-  const [colorToEdit, setColorToEdit] = useState(initialColor);
+  console.log("ColorList: colors:", colors)
+  const [editing, setEditing] = useState(false)
+  const [colorToEdit, setColorToEdit] = useState(initialColor)
 
   const editColor = color => {
-    setEditing(true);
-    setColorToEdit(color);
-  };
+    setEditing(true)
+    setColorToEdit(color)
+  }
 
   const saveEdit = e => {
-    e.preventDefault();
+    e.preventDefault()
     // Make a put request to save your updated color
     // think about where will you get the id from...
-    // where is is saved right now?
-  };
+    // where is it saved right now?
+    axiosWithAuth()
+      .put(`/colors`)
+      .then(res => {
+        console.log(res)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
 
   const deleteColor = color => {
     // make a delete request to delete this color
-  };
+  }
 
   return (
     <div className="colors-wrap">
@@ -79,7 +87,7 @@ const ColorList = ({ colors, updateColors }) => {
       <div className="spacer" />
       {/* stretch - build another form here to add a color */}
     </div>
-  );
-};
+  )
+}
 
-export default ColorList;
+export default ColorList
